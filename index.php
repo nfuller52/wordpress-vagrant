0a3107234a182a0c184fbbc337b1a458c6a50e71
Nick Fuller <nfuller52@gmail.com>
2015-08-18 08:45:53 -0700

    install composer, wpcs and xdebug

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <h1>Index</h1>
-<?php phpinfo();
+<?php
+/**
+ * IPC_Test_Config
+ *
+ * @category Testing
+ * @package  IPC.Test
+ * @author   Rajat Pandit <rajat_pandit@lalaland.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://ipcmedia.com
+ */
+
+/**
+ * IPC_Test_Config
+ *
+ * @category Testing
+ * @package  IPC.Test
+ * @author   Rajat Pandit <rajat_pandit@lalaland.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://ipcmedia.com
+ */
+
+phpinfo();
+
+$name = array( 'nick' => 32 );
